10de619f05aa246c1ac06f30c78fc9b95f6610db
zhoushengyuan_glb <zhoushengyuan_glb@qq.com>
2018-09-16 04:48:45 +0830

    你怕是失了智

--- src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
+++ src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWebResultcrudApplication {
 
 	public static void main(String[] args) {
+		//你怕是失了智
 		SpringApplication.run(SpringBootWebResultcrudApplication.class, args);
 	}
 }
