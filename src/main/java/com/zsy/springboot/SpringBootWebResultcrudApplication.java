f24e6b37e0e79ad28d1e59b124a6af82d26968e8
zhoushengyuan_glb <zhoushengyuan_glb@qq.com>
2018-09-16 05:29:11 +0830

    //测试成功

--- src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
+++ src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWebResultcrudApplication {
 
 	public static void main(String[] args) {
-		//测试成功
+		//你怕是失了智
 		SpringApplication.run(SpringBootWebResultcrudApplication.class, args);
 	}
 }
