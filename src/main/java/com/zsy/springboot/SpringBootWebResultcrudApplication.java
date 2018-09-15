7db3f71c372816f203fa704c5b215b994f236dad
zhoushengyuan_glb <zhoushengyuan_glb@qq.com>
2018-09-16 05:22:22 +0830

    你怕是失了智

--- src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
+++ src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWebResultcrudApplication {
 
 	public static void main(String[] args) {
-		//是的
+		//clone在在在测试
 		SpringApplication.run(SpringBootWebResultcrudApplication.class, args);
 	}
 }
