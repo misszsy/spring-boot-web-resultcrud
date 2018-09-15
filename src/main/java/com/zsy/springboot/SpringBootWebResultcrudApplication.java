5cdb421109a6731b8b9f5c244b2b0d5bbfa4ed73
zhoushengyuan_glb <zhoushengyuan_glb@qq.com>
2018-09-16 05:21:10 +0830

    clone测试

--- src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
+++ src/main/java/com/zsy/springboot/SpringBootWebResultcrudApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWebResultcrudApplication {
 
 	public static void main(String[] args) {
-		//是的
+		//clone测试
 		SpringApplication.run(SpringBootWebResultcrudApplication.class, args);
 	}
 }
